98ce867dbea4ae84210fd30f568131d6861d0722
EMachadoP <eldonmp2@gmail.com>
2026-02-11 22:28:48 -0300

    chore: cleanup temporary fix and debug scripts from project root

--- check_db_settings.py
+++
@@ -1,9 +0,0 @@
-import os
-import django
-from django.conf import settings
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'erp.settings')
-django.setup()
-
-print("DATABASES configuration:")
-print(settings.DATABASES)
